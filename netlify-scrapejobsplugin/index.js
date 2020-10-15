bd288cf609cc1487daf3d993213ad803fa4d53f6
opuerto <oscar.puertohn@gmail.com>
2020-10-15 22:25:47 +0200

    const

--- netlify-scrapejobsplugin/index.js
+++ netlify-scrapejobsplugin/index.js
@@ -1,16 +1,16 @@
 
 const chromium = require('chrome-aws-lambda')
 //Start the browser and create a browser instance
-const faunadb = require('faunadb'),
-
+const faunadb = require('faunadb')
 
 const url = 'https://rds-empleos.hn/plazas/';
+
 q = faunadb.query;
 
 // Once required, we need a new instance with our secret
 var adminClient = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
- });
+});
 
 module.exports = {
     onPreBuild: async () => {
@@ -29,24 +29,24 @@ module.exports = {
 
         const jobItem = {
             data: {
-                id:1,
-                company:"mercurio",
-                email:"fun@fun.com",
-                city:"tegucigalpa",
-                date:"22.22.20",
-                ur:"https/url",
-                published:false
+                id: 1,
+                company: "mercurio",
+                email: "fun@fun.com",
+                city: "tegucigalpa",
+                date: "22.22.20",
+                ur: "https/url",
+                published: false
             }
-          }
+        }
 
         console.log("Function `todo-create` invoked")
 
         try {
             const response = await client.query(q.Create(q.Ref("classes/jobs"), jobItem))
             console.log("job created");
-          } catch(err) {
+        } catch (err) {
             alert(err); // TypeError: failed to fetch
-          }
+        }
 
 
     },
