4b4a0ed356f3ef4035d7eeeca9443c02dc2c25d8
opuerto <oscar.puertohn@gmail.com>
2020-10-15 22:47:25 +0200

    create jobs

--- netlify-scrapejobsplugin/index.js
+++ netlify-scrapejobsplugin/index.js
@@ -58,8 +58,18 @@ module.exports = {
 
         console.log("Function `todo-create` invoked")
 
-        data.map(async (item) => {
-            const jobItem = { data: { item } }
+        data.map(async (job) => {
+            const jobItem = {
+                data: {
+                    id: job.id,
+                    company: job.company,
+                    email: job.email,
+                    city: job.city,
+                    date: job.date,
+                    ur: job.ur,
+                    published: job.published
+                }
+            }
             try {
                 const response = await adminClient.query(q.Create(q.Ref("classes/jobs"), jobItem))
                 console.log("job response", response);
