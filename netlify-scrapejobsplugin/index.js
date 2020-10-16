bf487d7d9d53045fcc4cbe9f015e8dc041b399b9
opuerto <oscar.puertohn@gmail.com>
2020-10-16 23:22:41 +0200

    filter

--- netlify-scrapejobsplugin/index.js
+++ netlify-scrapejobsplugin/index.js
@@ -17,16 +17,19 @@ var adminClient = new faunadb.Client({
 module.exports = {
     onPreBuild: async () => {
         //Start the browser and create a browser instance
-        // let browserInstance = browser.startBrowser();
-        // const getJobs = await scraperController(browserInstance);
-        // (await browserInstance).close();
-        // console.log("the jobs data", getJobs);
-        const response = await adminClient.query(q.Paginate(q.Match(q.Index("jobs_sort_by_first_desc"))));
+        let browserInstance = browser.startBrowser();
+        const getJobs = await scraperController(browserInstance);
+        (await browserInstance).close();
+        console.log("the jobs data", getJobs);
+        const JobsById = await adminClient.query(q.Paginate(q.Match(q.Index("jobs_sort_by_first_desc"))));
         const ids = [];
-        response.data.map((job) => {
-            ids.push(job[0]);
-        })
-        console.log("data", ids);
+        const filteredData = getJobs.filter((job) => JobsById.some((jobByID) => job.JobID !== jobByID[0]));
+
+        // response.data.map((job) => {
+        //     ids.push(job[0]);
+        // })
+        console.log("Filtered data", filteredData);
+        console.log("filtered data size", filteredData.lenght);
 
     },
 }
