67f4ef45c42719ac05a1c084774a928018c3f901
opuerto <oscar.puertohn@gmail.com>
2020-10-16 12:20:30 +0200

    s

--- netlify-scrapejobsplugin/index.js
+++ netlify-scrapejobsplugin/index.js
@@ -1,87 +1,84 @@
 
-const chromium = require('chrome-aws-lambda')
-//Start the browser and create a browser instance
-const faunadb = require('faunadb')
+// const chromium = require('chrome-aws-lambda')
+// //Start the browser and create a browser instance
+// const faunadb = require('faunadb')
 
-const FB = require('fb');
+// const FB = require('fb');
 
-const url = 'https://rds-empleos.hn/plazas/';
+// const url = 'https://rds-empleos.hn/plazas/';
 
-q = faunadb.query;
+// q = faunadb.query;
 
-// Once required, we need a new instance with our secret
-var adminClient = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-});
+// // Once required, we need a new instance with our secret
+// var adminClient = new faunadb.Client({
+//     secret: process.env.FAUNADB_SERVER_SECRET
+// });
 
 module.exports = {
     onPreBuild: async () => {
-        const executablePath = await chromium.executablePath
-        const browser = await chromium.puppeteer.launch({
-            args: await chromium.args,
-            executablePath: executablePath || process.env.PUPPETEER_EXECUTABLE_PATH,
-            headless: true,
-        })
-        const page = await browser.newPage();
-
-        await page.goto(url);
-        const screenshot = await page.screenshot({ encoding: 'binary' });
-
-        await browser.close();
-        const data = [{
-            id: 1,
-            company: "mercurio",
-            email: "fun@fun.com",
-            city: "tegucigalpa",
-            date: "22.22.20",
-            ur: "https/url",
-            published: false
-        }, {
-            id: 2,
-            company: "mercurio2",
-            email: "fun@fun.com",
-            city: "tegucigalpa",
-            date: "22.22.20",
-            ur: "https/url",
-            published: false
-        }]
-
-
-        console.log("Function `todo-create` invoked")
-
-        // data.map(async (job) => {
-        //     const jobItem = {
-        //         data: {
-        //             id: job.id,
-        //             company: job.company,
-        //             email: job.email,
-        //             city: job.city,
-        //             date: job.date,
-        //             ur: job.ur,
-        //             published: job.published
-        //         }
-        //     }
-        //     try {
-        //         const response = await adminClient.query(q.Create(q.Ref("classes/jobs"), jobItem))
-        //         console.log("job response", response);
-        //     } catch (err) {
-        //         console.log(err)
-        //     }
+        // const executablePath = await chromium.executablePath
+        // const browser = await chromium.puppeteer.launch({
+        //     args: await chromium.args,
+        //     executablePath: executablePath || process.env.PUPPETEER_EXECUTABLE_PATH,
+        //     headless: true,
         // })
+        // const page = await browser.newPage();
 
-        console.log("job created");
-        let facebook;
-        // facebook feed 
+        // await page.goto(url);
+        // const screenshot = await page.screenshot({ encoding: 'binary' });
+
+        // await browser.close();
+        // const data = [{
+        //     id: 1,
+        //     company: "mercurio",
+        //     email: "fun@fun.com",
+        //     city: "tegucigalpa",
+        //     date: "22.22.20",
+        //     ur: "https/url",
+        //     published: false
+        // }, {
+        //     id: 2,
+        //     company: "mercurio2",
+        //     email: "fun@fun.com",
+        //     city: "tegucigalpa",
+        //     date: "22.22.20",
+        //     ur: "https/url",
+        //     published: false
+        // }]
+
+
+        // console.log("Function `todo-create` invoked")
+
+        // // data.map(async (job) => {
+        // //     const jobItem = {
+        // //         data: {
+        // //             id: job.id,
+        // //             company: job.company,
+        // //             email: job.email,
+        // //             city: job.city,
+        // //             date: job.date,
+        // //             ur: job.ur,
+        // //             published: job.published
+        // //         }
+        // //     }
+        // //     try {
+        // //         const response = await adminClient.query(q.Create(q.Ref("classes/jobs"), jobItem))
+        // //         console.log("job response", response);
+        // //     } catch (err) {
+        // //         console.log(err)
+        // //     }
+        // // })
+
+        var FB = require('fb');
+
         FB.setAccessToken('EAAE8cHi7s9oBAO0MiBlVx6B5hmnAhZCVBbZBlu6uIJIhdrZBjZC0DD8OayuDmx9SroMhU8xp9dKisHleAkmlQZAq9tqLfCzG3p00jP0y7sFwKEMmXZCVYFwys03o6uxfKL0iseZBvSsqWbLJAfka269wxMMXcHbqDffYEZARiCf37XnKVrJbJ3TBrnuZC0ZAycseAZD');
         FB.api(
             '/mercuriohonduras/feed',
             'POST',
-            { "message": "Testing with api from netlify" },
+            { "message": "Testing with api met netlify" },
             function (response) {
-                facebook = response;
-                console.log("facebook post", response);
+                console.log(response);
             }
-
         );
 
         console.log("facebook hit", facebook);
